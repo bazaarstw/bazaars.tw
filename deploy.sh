c675b189b1f0298ebfd67592f58216e440cace6b
yan <a0726h77@gmail.com>
2015-08-13 10:35:58 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-lftp -u $FTP_USER,$FTP_PASSWORD -e "set ssl:verify-certificate no; mirror -Rev -X .git . web_bazaars/. ; exit" ftp://$FTP_HOST/
+lftp -u $FTP_USER,$FTP_PASSWORD -e "set ftp:ssl-allow no; set ssl:verify-certificate no; mirror -Rev -x .git . web_bazaars/. ; exit" ftp://$FTP_HOST/
