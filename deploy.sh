d8fbaee312325dd03eb354439e45c3f6a8d56952
yan <a0726h77@gmail.com>
2015-08-13 09:52:32 +0800

    update .travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-lftp -u $FTP_USER,$FTP_PASSWORD -e "set ftp:ssl-allow no; mirror -R -e . web_bazaars ; exit" ftp://$FTP_HOST/
+lftp -u $FTP_USER,$FTP_PASSWORD -e "set ftp:ssl-allow no; set ssl:verify-certificate no; mirror -R -e . web_bazaars ; exit" ftp://$FTP_HOST/
